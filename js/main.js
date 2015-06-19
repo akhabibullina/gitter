2c6ad75f5e3ce5725ba2b5366906928ded14a2de
akhabibullina <akhabibullina@aisquared.com>
2015-06-19 18:47:12 -0400

    Fix the naming error(github).

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ require.config({
     jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
     underscore: 'lib/underscore',
     backbone: 'lib/backbone',
-    localstorage: 'lib/backbone.localStorage',
+    localstorage: 'lib/backbone.localstorage',
     template: '../templates',
     tests: 'tests',
     log: 'log'
